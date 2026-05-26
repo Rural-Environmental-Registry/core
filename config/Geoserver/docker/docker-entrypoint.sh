d31faaa2f8fdce96406facf33193e1c8d28411fb
guilhermeaalves317 <guilherme.alves@youxgroup.com.br>
2026-05-26 17:28:03 -0300

    fix: ubuntu user on entrypoint and configs for Ngnix and Geoserver

--- config/Geoserver/docker/docker-entrypoint.sh
+++ config/Geoserver/docker/docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Ajusta permissões do data dir (volume pode ter sido criado como root) e inicia o GeoServer como UID 1000.
+# Ajusta permissões do data dir (volume pode ter sido criado como root).
+# O startup.sh oficial aplica RUN_UNPRIVILEGED e sobe o Tomcat com setpriv.
 set -e
 
-GEOSERVER_UID="${GEOSERVER_UID:-1000}"
-GEOSERVER_GID="${GEOSERVER_GID:-1000}"
+GEOSERVER_UID="${RUN_WITH_USER_UID:-${GEOSERVER_UID:-999}}"
+GEOSERVER_GID="${RUN_WITH_USER_GID:-${GEOSERVER_GID:-999}}"
 DATA_DIR="${GEOSERVER_DATA_DIR:-/var/geoserver/datadir}"
 
 mkdir -p "$DATA_DIR"
 chown -R "${GEOSERVER_UID}:${GEOSERVER_GID}" "$DATA_DIR"
 
-exec runuser -u ubuntu -- /opt/startup.sh "$@"
+exec /opt/startup.sh "$@"
